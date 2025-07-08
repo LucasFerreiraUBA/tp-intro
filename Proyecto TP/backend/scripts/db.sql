1db3e659d8ff60c0c535ee000c0e825a9d0dc334
tiago <tsedano@fi.uba.ar>
2025-07-08 11:59:54 -0300

    agregue un punto y coma que le faltaba a la tabla de entrenamiento por lo que no funcionaba al levantar el backend

--- Proyecto TP/backend/scripts/db.sql
+++ Proyecto TP/backend/scripts/db.sql
@@ -109,7 +109,7 @@ INSERT INTO entrenamiento (dia_semana, objetivo, nivel_usuario, duracion_minutos
 ('Jueves', 'Espalda y bíceps', 'Intermedio', 65, 'Tirón y brazos'),
 ('Viernes', 'Fullbody', 'Principiante', 60, 'Circuito de cuerpo completo'),
 ('Sábado', 'Alta intensidad HIIT', 'Avanzado', 40, 'Interválico con ejercicios compuestos'),
-('Domingo', 'Recuperación activa', 'Todos', 30, 'Movilidad, estiramientos y core'),
+('Domingo', 'Recuperación activa', 'Todos', 30, 'Movilidad, estiramientos y core');
 
 INSERT INTO entrenamiento_ejercicio (entrenamiento_id, rutina_id) VALUES
 (1, 1), (1, 5), (1, 7),
