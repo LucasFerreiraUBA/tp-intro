e561f3d655c48cf7ed513a66010a01f076bfbace
Tomas <tdiaz@fi.uba.ar>
2025-07-19 14:25:16 -0300

    Agregue validaciones

--- Proyecto TP/backend/scripts/ejercicios.js
+++ Proyecto TP/backend/scripts/ejercicios.js
@@ -81,6 +81,41 @@ async function createEjercicio(
       [grupo_muscular_nombre]
     );
 
+    if (descripcion.length > 100) {
+      console.error("La descripción es demasiado larga");
+      return null; 
+    } else if (ejercicio.length > 50) {
+      console.error("El nombre del ejercicio es demasiado largo");
+      return null;
+    } else if (repeticiones < 1 || series < 1) {
+      console.error("Las repeticiones y series deben ser al menos 1");
+      return null; 
+    } else if (repeticiones > 100 || series > 100) {
+      console.error("Las repeticiones y series no pueden ser mayores a 100");
+      return null;
+    } else if (peso < 0) {
+      console.error("El peso no puede ser negativo");  
+      return null; 
+    } else if (peso > 2000) {
+      console.error("El peso no puede ser mayor a 2000");
+      return null; 
+    } else if (tiempo_descanso < 0) {
+      console.error("El tiempo de descanso no puede ser negativo");
+      return null; 
+    } else if (tiempo_descanso > 1000) {
+      console.error("El tiempo de descanso es demasiado largo");
+      return null; 
+    } else if (isNaN(series) || isNaN(repeticiones)) {
+      alert("Las series y repeticiones deben ser números válidos.");
+      return; 
+    } else if (isNaN(peso)) {
+      alert("El peso debe ser un número válido.");
+      return;
+    } else if (isNaN(rir)) {
+      alert("El RIR debe ser un número válido.");
+      return; 
+    }
+
     if (grupoResult.rowCount === 0) {
       return null;
     }
